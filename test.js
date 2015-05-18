192932aab207347befede86166601d5a4fe83161
Jason Orendorff <jason.orendorff@gmail.com>
2015-05-18 17:23:33 -0500

    Add the game of Fifteen.

--- test.js
+++ test.js
@@ -64,8 +64,6 @@ function _annotateGame(game, lastPlayer, otherPlayer) {
 
 function TicTacToe(board) {
     this.board = board;
-    if (this.board.length !== 9)
-        debugger;
 }
 
 TicTacToe.start = new TicTacToe("         ");
@@ -182,6 +180,73 @@ Pennies.prototype.toString = function () {
 
 
 
+// === Another simple game: Fifteen
+// The game starts with the numbers 1 to 9 all sitting on the table.
+// Players take turns taking a number. The goal is to pick the right numbers
+// so that some set of exactly three of your numbers adds up to 15.
+
+function Fifteen(p0, p1) {
+    this.players = [p0, p1];
+
+    function has(p, n) { return p.indexOf(n) !== -1; }
+
+    this._winner = undefined;
+    for (var a = 1; a <= 7; a++) {
+        for (var b = a + 1; b <= 8; b++) {
+            var c = 15 - (a + b);
+            if (c > b && c <= 9) {
+                if (has(p0, a) && has(p0, b) && has(p0, c)) {
+                    this._winner = 0;
+                    break;
+                } else if (has(p1, a) && has(p1, b) && has(p1, c)) {
+                    this._winner = 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (this._winner === undefined) {
+        this._moves = [1, 2, 3, 4, 5, 6, 7, 8, 9].filter(function (move) {
+            return p0.indexOf(move) === -1 && p1.indexOf(move) === -1;
+        });
+    } else {
+        this._moves = [];
+    }
+}
+
+Fifteen.start = new Fifteen([], []);
+
+Fifteen.prototype.moves = function () { return this._moves; };
+
+Fifteen.prototype.scoreFinishedGame = function () {
+    if (this._winner === undefined)
+        return 0;
+    else
+        return +1;  // winner is always the player who moved last
+};
+
+Fifteen.prototype.applyMove = function (move) {
+    var p0 = this.players[0];
+    var p1 = this.players[1];
+
+    if (p0.length === p1.length) {
+        p0 = p0.concat(move);
+    } else {
+        p1 = p1.concat(move);
+    }
+    return new Fifteen(p0, p1);
+};
+
+Fifteen.prototype.toString = function () {
+    return ("Your numbers: " + this.players[0].join(", ") + "\n" +
+            "My numbers: " + this.players[1].join(", ") + "\n" +
+            "Numbers still on the table: " + this._moves.join(", ") + "\n");
+};
+
+
+
+
 // === Some actual AI code
 
 // All game objects have these methods:
@@ -227,6 +292,8 @@ assert(scoreMove(new Pennies(3), 1) === -1);
 assert(bestMoveAndScore(new Pennies(3)).move === 3);
 assert(bestMoveAndScore(new Pennies(1)).score === 1);
 assert(bestMoveAndScore(new Pennies(4)).score === -1);
+
+assert(bestMoveAndScore(new Fifteen([7, 5], [1])).move === 3);
 
 
 
